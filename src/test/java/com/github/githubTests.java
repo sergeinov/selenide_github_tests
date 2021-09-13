eb05c1a1d3bf80ea04164dbe15de58f7b11f3a28
SergeyAN <lookbugs@mail.ru>
2021-09-13 15:05:34 +0300

    refactoring steps

--- src/test/java/com/github/githubTests.java
+++ src/test/java/com/github/githubTests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class githubTests {
@@ -19,19 +20,17 @@ public class githubTests {
     void searchSelenideTest() {
         // Step 1: Open a page "https://github.com/"
         open("https://github.com/");
-        // Step 2: Enter "selenide" in the search field
-        $("input[data-test-selector='nav-search-input']").setValue("selenide");
-        // Step 3: Press Enter
-        $("input[data-test-selector='nav-search-input']").pressEnter();
-        // Step 4: Open the repository "selenide/selenide"
-        $$("ul.repo-list li").first().$("a").click();
-        // Step 5: Navigate to Wiki of project
+        // Step 2: Enter "selenide" in the search field and Press Enter
+        $("input[data-test-selector='nav-search-input']").setValue("selenide").pressEnter();
+        // Step 3: Open the repository "selenide/selenide"
+        $("ul.repo-list li a").click();
+        // Step 4: Navigate to Wiki of project
         $("#wiki-tab").click();
-        // Step 6: Check: on the list "Page" have a page "Soft assertions"
+        // Step 5: Check: on the list "Page" have a page "Soft assertions"
         $(".wiki-pages-box button").shouldHave(text("more pages")).scrollIntoView(true).click();             // open the list
-        // Step 7: Open the page "Soft assertion"
+        // Step 6: Open the page "Soft assertion"
         $$(".wiki-pages-box ul li").findBy(text("SoftAssertions")).click();
-        // Step 8: Check that page contains example the code for Junit5
-        $$(".markdown-body ol li").findBy(exactText("Using JUnit5 extend test class:")).shouldBe(visible).scrollIntoView(true);
+        // Step 7: Check that page contains example the code for Junit5
+        $$(".markdown-body ol li").findBy(exactText("Using JUnit5 extend test class:")).shouldBe(visible);
     }
 }
