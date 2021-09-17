29597b9d6b3a78007c14d7ec98fb142058e349a5
SergeyAN <lookbugs@mail.ru>
2021-09-17 20:56:52 +0300

    refactoring Step 6

--- src/test/java/com/github/githubTests.java
+++ src/test/java/com/github/githubTests.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class githubTests {
@@ -29,7 +28,7 @@ public class githubTests {
         // Step 5: Check: on the list "Page" have a page "Soft assertions"
         $(".wiki-pages-box button").shouldHave(text("more pages")).scrollIntoView(true).click();             // open the list
         // Step 6: Open the page "Soft assertion"
-        $(".wiki-pages-box ul").$(byText("SoftAssertions")).click();
+        $$(".wiki-pages-box li").find(text("SoftAssertions")).click();
         // Step 7: Check that page contains example the code for Junit5
         $$(".markdown-body ol li").findBy(exactText("Using JUnit5 extend test class:")).shouldBe(visible);
     }
